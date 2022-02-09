0354ac83b8509d0673249d44dc328023a971f090
Adem <adem_demma@hotmail.com>
2022-02-09 23:58:43 +0100

    javascript homework

--- JavaScript/vezba.js
+++ JavaScript/vezba.js
@@ -630,29 +630,33 @@
 //   }
 // console.log(_keys({red: "#FF0000", green: "#00FF00", white: "#FFFFFF"}));
 
-// var library = [
-//   {
-//     title: "Bill Gates",
-//     author: "The Road Ahead",
-//     readingStatus: true,
-//   },
-//   {
-//     title: "Steve Jobs",
-//     author: "Walter Isaacson",
-//     readingStatus: true,
-//   },
-//   {
-//     title: "Mockingjay: The Final Book of The Hunger Games",
-//     author: "Suzanne Collins",
-//     readingStatus: false,
-//   },
-// ];
 
-// for (var i = 0; i < library.length; i++) {
-//   var book = "'" + library[i].title + "'" + " by " + library[i].author + ".";
-//   if (library[i].readingStatus) {
-//     console.log("Already read " + book);
-//   } else {
-//     console.log("You still need to read " + book);
-//   }
-// }
+
+var library = [
+  {
+    title: "Bill Gates",
+    author: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    title: "Steve Jobs",
+    author: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    readingStatus: false,
+  },
+];
+
+
+
+for (var i = 0; i < library.length; i++) {
+  var book = "'" + library[i].title + "'" + " by " + library[i].author + ".";
+  if (library[i].readingStatus) {
+    console.log("Already read " + book);
+  } else {
+    console.log("You still need to read " + book);
+  }
+}
