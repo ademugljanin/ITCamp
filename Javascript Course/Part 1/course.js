734c1860f76b0022890b056b5d7f31954d11f2ae
Adem <adem_demma@hotmail.com>
2022-02-16 16:11:45 +0100

    Javascript Course

--- Javascript Course/Part 1/course.js
+++ Javascript Course/Part 1/course.js
@@ -201,5 +201,7 @@
 //         console.log('Not a valid day!');
 //     }
 
+// var bill = Number(prompt("Enter your bill:"));
+// var tip = bill <= 300 && bill >=50 ? bill *0.15 : bill*0.2;
 
-
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill+tip}`);
